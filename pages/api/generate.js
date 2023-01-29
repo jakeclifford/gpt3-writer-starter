86db52193f2bb18189bc0554ca4d408d7d82ecfe
Jake Clifford <jakeclifford35@gmail.com>
2023-01-29 14:09:02 +1100

    Improved prompt format

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,12 +13,22 @@ const generateAction = async (req, res) => {
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.apiCall} in the format 
-    1.activity: description
-    2.activity: description
-    3.activity: description
-    4.activity: description
-    5.activity: description`,
+    prompt: `
+    prompt: Unique and detailed travel ideas in taiwan if you like fishing
+    response:
+    1. Go Saltwater fly-fishing in Kinmen: Kinmen is an archipelago located just off the coast of mainland China that offers excellent saltwater fly-fishing opportunities. You can target a variety of species including trevally, queenfish, and barracuda.
+
+    2.Visit Taiwan's east coast: Taiwan's east coast has a diverse range of fish species, from large game fish like tuna and marlin to smaller species like grouper and snapper. You can book a fishing charter to explore the best spots for these fish.
+
+    3.Experiance Trout fishing in the Central Mountains: Taiwan's central mountain range offers great trout fishing in its crystal clear mountain streams and rivers. The most popular species to catch is the Taiwan native "Formosan landlocked salmon"
+
+    4.Take an adventure Kayak fishing in Tainan: Tainan is known for its mangrove forests, estuaries and coastal lagoons that are ideal for kayak fishing. You can target species like snook, tarpon, and mangrove jack.
+
+    5.Have the time of your life Fishing on the Sun Moon Lake: Sun Moon Lake is known for its scenic beauty and is a great spot to catch fish like mandarin fish, tilapia and carp. You can also hire a boat and try to catch the Giant Snakehead (Channa argus) a freshwater fish native to Taiwan.
+    
+    prompt: Unique and detailed travel ideas in ${req.body.apiCall}
+    response:
+    `,
     temperature: 0.9,
     max_tokens: 250,
   });
